40792af48ce88229dec42f54b1dd376d112be2ab
Dominique Sakrisson <domsakrisson@gmailcom>
2021-01-08 23:12:30 -0800

    include google charts

---
+++ app.js
@@ -0,0 +1,4 @@
+// Load Charts and the bar package.
+google.charts.load("current", { packages: ["bar"] });
+// Draw the bar charts when charts is loaded.
+google.charts.setOnLoadCallback(drawChart);
